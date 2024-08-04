cd0bdc8c1f2c212b74884fcd911db5069855cfbb
43a1723 <hai1723notgay@gmail.com>
2024-08-04 18:22:05 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 pip install --upgrade nuitka
-python -m nuitka --standalone --onefile --follow-imports --windows-icon-from-ico=skid.png --output-dir=dist --output-filename=skid.exe skid.py
+python -m nuitka --standalone --onefile --follow-imports --output-dir=dist --output-filename=skid.exe skid.py
